2e1774adc1ba1d2d66f2a3d4fa721b16292a59e7
Manuel Gonçalves <mjgoncalves.trabalho@gmail.com>
2022-03-31 17:39:22 +0100

    Functional version of the caesar cipher

---
+++ caesar_cipher.rb
@@ -0,0 +1,40 @@
+def caesar_cipher(string, integer)
+
+  phrase = []
+  ascii = []
+  final_letters = []
+  a_to_z = ("A"..."Z").to_a + ("a"..."z").to_a
+  a_to_z.push('z')
+  a_to_z.push('Z')
+
+  string.each_char do | char |
+    phrase.push(char)
+  end
+
+  phrase.each do |char|
+    if a_to_z.include?(char)
+      ascii.push(char.ord)
+      next
+    else
+    ascii.push(char)
+    end
+  end
+
+  ascii.each do | ascii_code |
+    if a_to_z.include?(ascii_code.chr)
+      letter = ascii_code.to_i + integer
+      final_letters.push(letter.chr)
+    else
+      final_letters.push(ascii_code)
+    end
+  end
+
+    final_letters.join()
+end
+
+puts "Hello, please enter a phrase to be ciphered"
+phrase = gets.chomp
+puts "Hello again, please enter a number"
+number = gets.chomp.to_i
+
+puts caesar_cipher(phrase, number)
